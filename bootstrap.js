10e4a16dabeffbd5398f76a86e60ba9d41d17fb3
reggev am <barak.eden@devalore.com>
2019-08-08 13:34:40 +0300

    added production environment setup

--- bootstrap.js
+++ bootstrap.js
@@ -1,4 +1,13 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: './.env.production' });
+  console.info('running in production');
+} else {
+  dotenv.config();
+  console.info('running in development');
+}
+
 const { server } = require('./src');
 
 server.listen().then(({ url }) => {
