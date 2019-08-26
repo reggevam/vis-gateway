40f8f0866543c166305c142b8d701021d75a9f67
reggev am <barak.eden@devalore.com>
2019-08-26 10:27:14 +0300

    dropped redundent log

--- bootstrap.js
+++ bootstrap.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 
 const { server } = require('./src');
 
-console.log(process.env.NER_URL);
-
 server.listen().then(({ url }) => {
   console.info(`🚀 listening on ${url}`);
 });
